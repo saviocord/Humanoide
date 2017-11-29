d9f8590593d0b9ffb8dead3f34ae4a22091723a6
Sávio Cordeiro <saviocord@gmail.com>
2017-11-29 20:44:13 -0200

    ajuste na rotação

--- main.cpp
+++ main.cpp
@@ -7,17 +7,24 @@
 #include <GL/freeglut.h>
 
 
-enum {HELP, PESCOCO, ABDOMEN,
+enum {MENU, PESCOCO, ABDOMEN,
       BRACO_D, ANTEBRACO_D, QUADRIL_D, COXA_D, CANELA_D, PE_D,
       BRACO_E, ANTEBRACO_E, QUADRIL_E, COXA_E, CANELA_E, PE_E};
 
 GLfloat rotX, rotY, rotZ; //variaveis para a camera
-GLfloat xRot, yRot, zRot = 1.0; //variaveis para o boneco
-int operacao = HELP;
+int xRot, yRot, zRot = 1; //variaveis para o boneco
+
+int operacao = MENU;
 GLfloat lightpos[] = {-25.f, 0.f, 50.f, 1.f};
 
-static int pescocoRot = 0, bracoERot = 0, antebracoERot = 0, bracoDRot = 0, antebracoDRot = 0, abdomenRot = 0;
-static int quadrilDRot = 0, quadrilERot = 0,coxaERot = 0, coxaDRot = 0, canelaERot = 0, canelaDRot = 0, peERot = 0, peDRot = 0;
+static int pescocoRotX = 0, bracoERotX = 0, antebracoERotX = 0, bracoDRotX = 0, antebracoDRotX = 0, abdomenRotX = 0;
+static int quadrilDRotX = 0, quadrilERotX = 0,coxaERotX = 0, coxaDRotX = 0, canelaERotX = 0, canelaDRotX = 0, peERotX = 0, peDRotX = 0;
+
+static int pescocoRotY = 0, bracoERotY = 0, antebracoERotY = 0, bracoDRotY = 0, antebracoDRotY = 0, abdomenRotY = 0;
+static int quadrilDRotY = 0, quadrilERotY = 0,coxaERotY = 0, coxaDRotY = 0, canelaERotY = 0, canelaDRotY = 0, peERotY = 0, peDRotY = 0;
+
+static int pescocoRotZ = 0, bracoERotZ = 0, antebracoERotZ = 0, bracoDRotZ = 0, antebracoDRotZ = 0, abdomenRotZ = 0;
+static int quadrilDRotZ = 0, quadrilERotZ = 0,coxaERotZ = 0, coxaDRotZ = 0, canelaERotZ = 0, canelaDRotZ = 0, peERotZ = 0, peDRotZ = 0;
 
 struct Posicao {
     float x, y, z;
@@ -63,10 +70,12 @@ void drawBitmapText(char *string,float x,float y,float z)
     glPopMatrix();
 }
 
-void aplicaRotacao(Posicao parte, int rotacao)
+void aplicaRotacao(Posicao parte, int x, int y, int z)
 {
     glTranslatef(parte.x, parte.y, parte.z);
-    glRotatef ((GLfloat) rotacao, xRot, yRot, zRot);
+    glRotatef ((GLfloat) x, 1, 0, 0);
+    glRotatef ((GLfloat) y, 0, 1, 0);
+    glRotatef ((GLfloat) z, 0, 0, 1);
     glTranslatef(-parte.x, -parte.y, -parte.z);
 }
 
@@ -94,7 +103,6 @@ void desenhaLinha(Posicao parteIni, Posicao parteFim, bool selecionado = false)
 }
 
 void draw() {
-    /* clear stencil each time */
     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
@@ -111,27 +119,27 @@ void draw() {
     glLineWidth(3);
 
     glPushMatrix();
-    aplicaRotacao(virilha, abdomenRot);
+    aplicaRotacao(virilha, abdomenRotX, abdomenRotY, abdomenRotZ);
     desenhaPonto(tronco, ((operacao == ABDOMEN)||(operacao == PESCOCO)||(operacao == BRACO_D)||(operacao == BRACO_E)));
 
     glPushMatrix();
-    aplicaRotacao(tronco, pescocoRot);
+    aplicaRotacao(tronco, pescocoRotX, pescocoRotY, pescocoRotZ);
     desenhaPonto(cabeca, (operacao == PESCOCO));
     glPopMatrix();
 
     glPushMatrix();
-    aplicaRotacao(tronco, bracoERot);
+    aplicaRotacao(tronco, bracoERotX, bracoERotY, bracoERotZ);
     desenhaPonto(cotoveloE, ((operacao == BRACO_E)||(operacao == ANTEBRACO_E)));
 
-    aplicaRotacao(cotoveloE, antebracoERot);
+    aplicaRotacao(cotoveloE, antebracoERotX, antebracoERotY, antebracoERotZ);
     desenhaPonto(maoE, (operacao == ANTEBRACO_E));
     glPopMatrix();
 
     glPushMatrix();
-    aplicaRotacao(tronco, bracoDRot);
+    aplicaRotacao(tronco, bracoDRotX, bracoDRotY, bracoDRotZ);
     desenhaPonto(cotoveloD, ((operacao == BRACO_D)||(operacao == ANTEBRACO_D)));
 
-    aplicaRotacao(cotoveloD, antebracoDRot);
+    aplicaRotacao(cotoveloD, antebracoDRotX, antebracoDRotY, antebracoDRotZ);
     desenhaPonto(maoD, (operacao == ANTEBRACO_D));
     glPopMatrix();
     glPopMatrix();
@@ -140,18 +148,18 @@ void draw() {
     desenhaPonto(virilha, ((operacao == ABDOMEN)||(operacao == QUADRIL_D)||(operacao == QUADRIL_E)));
 
     glPushMatrix();
-    aplicaRotacao(virilha, quadrilDRot);
+    aplicaRotacao(virilha, quadrilDRotX, quadrilDRotY, quadrilDRotZ);
     desenhaPonto(virilhaD, ((operacao == QUADRIL_D)||(operacao == COXA_D)));
 
     glPushMatrix();
-    aplicaRotacao(virilhaD, coxaDRot);
+    aplicaRotacao(virilhaD, coxaDRotX, coxaDRotY, coxaDRotZ);
     desenhaPonto(joelhoD, ((operacao == COXA_D)||(operacao == CANELA_D)));
 
     glPushMatrix();
-    aplicaRotacao(joelhoD, canelaDRot);
+    aplicaRotacao(joelhoD, canelaDRotX, canelaDRotY, canelaDRotZ);
     desenhaPonto(tornozeloD, ((operacao == CANELA_D)||(operacao == PE_D)));
 
-    aplicaRotacao(tornozeloD, peDRot);
+    aplicaRotacao(tornozeloD, peDRotX, peDRotY, peDRotZ);
     desenhaPonto(peD, (operacao == PE_D));
     glPopMatrix();
 
@@ -159,18 +167,18 @@ void draw() {
     glPopMatrix();
 
     glPushMatrix();
-    aplicaRotacao(virilha, quadrilERot);
+    aplicaRotacao(virilha, quadrilERotX, quadrilERotY, quadrilERotZ);
     desenhaPonto(virilhaE, ((operacao == QUADRIL_E)||(operacao == COXA_E)));
 
     glPushMatrix();
-    aplicaRotacao(virilhaE, coxaERot);
+    aplicaRotacao(virilhaE, coxaERotX, coxaERotY, coxaERotZ);
     desenhaPonto(joelhoE, ((operacao == COXA_E)||(operacao == CANELA_E)));
 
     glPushMatrix();
-    aplicaRotacao(joelhoE, canelaERot);
+    aplicaRotacao(joelhoE, canelaERotX, canelaERotY, canelaERotZ);
     desenhaPonto(tornozeloE, ((operacao == CANELA_E)||(operacao == PE_E)));
 
-    aplicaRotacao(tornozeloE, peERot);
+    aplicaRotacao(tornozeloE, peERotX, peERotY, peERotZ);
     desenhaPonto(peE, (operacao == PE_E));
 
     glPopMatrix();
@@ -182,44 +190,44 @@ void draw() {
 
     /*----------------------LINHAS------------------------------------------*/
     glPushMatrix();
-    aplicaRotacao(virilha, abdomenRot);
+    aplicaRotacao(virilha, abdomenRotX, abdomenRotY, abdomenRotZ);
     desenhaLinha(tronco, virilha, operacao == ABDOMEN);
 
     glPushMatrix();
-    aplicaRotacao(tronco, pescocoRot);
+    aplicaRotacao(tronco, pescocoRotX, pescocoRotY, pescocoRotZ);
     desenhaLinha(cabeca, tronco, operacao == PESCOCO);
     glPopMatrix();
 
     glPushMatrix();
-    aplicaRotacao(tronco, bracoERot);
+    aplicaRotacao(tronco, bracoERotX, bracoERotY, bracoERotZ);
     desenhaLinha(tronco, cotoveloE, operacao == BRACO_E);
 
-    aplicaRotacao(cotoveloE, antebracoERot);
+    aplicaRotacao(cotoveloE, antebracoERotX, antebracoERotY, antebracoERotZ);
     desenhaLinha(cotoveloE, maoE, operacao == ANTEBRACO_E);
     glPopMatrix();
 
     glPushMatrix();
-    aplicaRotacao(tronco, bracoDRot);
+    aplicaRotacao(tronco, bracoDRotX, bracoDRotY, bracoDRotZ);
     desenhaLinha(tronco, cotoveloD, operacao == BRACO_D);
 
-    aplicaRotacao(cotoveloD, antebracoDRot);
+    aplicaRotacao(cotoveloD, antebracoDRotX, antebracoDRotY, antebracoDRotZ);
     desenhaLinha(cotoveloD, maoD, operacao == ANTEBRACO_D);
     glPopMatrix();
     glPopMatrix();
 
     glPushMatrix();
-    aplicaRotacao(virilha, quadrilDRot);
+    aplicaRotacao(virilha, quadrilDRotX, quadrilDRotY, quadrilDRotZ);
     desenhaLinha(virilha, virilhaD, operacao == QUADRIL_D);
 
     glPushMatrix();
-    aplicaRotacao(virilhaD, coxaDRot);
+    aplicaRotacao(virilhaD, coxaDRotX, coxaDRotY, coxaDRotZ);
     desenhaLinha(virilhaD, joelhoD, operacao == COXA_D);
 
     glPushMatrix();
-    aplicaRotacao(joelhoD, canelaDRot);
+    aplicaRotacao(joelhoD, canelaDRotX, canelaDRotY, canelaDRotZ);
     desenhaLinha(joelhoD, tornozeloD, operacao == CANELA_D);
 
-    aplicaRotacao(tornozeloD, peDRot);
+    aplicaRotacao(tornozeloD, peDRotX, peDRotY, peDRotZ);
     desenhaLinha(tornozeloD, peD, operacao == PE_D);
     glPopMatrix();
 
@@ -227,18 +235,18 @@ void draw() {
     glPopMatrix();
 
     glPushMatrix();
-    aplicaRotacao(virilha, quadrilERot);
+    aplicaRotacao(virilha, quadrilERotX, quadrilERotY, quadrilERotZ);
     desenhaLinha(virilha, virilhaE, operacao == QUADRIL_E);
 
     glPushMatrix();
-    aplicaRotacao(virilhaE, coxaERot);
+    aplicaRotacao(virilhaE, coxaERotX, coxaERotY, coxaERotZ);
     desenhaLinha(virilhaE, joelhoE, operacao == COXA_E);
 
     glPushMatrix();
-    aplicaRotacao(joelhoE, canelaERot);
+    aplicaRotacao(joelhoE, canelaERotX, canelaERotY, canelaERotZ);
     desenhaLinha(joelhoE, tornozeloE, operacao == CANELA_E);
 
-    aplicaRotacao(tornozeloE, peERot);
+    aplicaRotacao(tornozeloE, peERotX, peERotY, peERotZ);
     desenhaLinha(tornozeloE, peE, operacao == PE_E);
     glPopMatrix();
 
@@ -251,7 +259,6 @@ void draw() {
     glutSwapBuffers();
 }
 
-/* special keys, like array and F keys */
 void specialKey(int key, int, int) {
     switch(key) {
         case GLUT_KEY_LEFT:
@@ -274,119 +281,135 @@ void specialKey(int key, int, int) {
 }
 
 void rotaciona(char key){
+    int angulo = key == '+' ? 5 : -5;
+
+    std::cout<<"x:"<<xRot<<" y:"<<yRot<<" z:"<<zRot<<std::endl;
+
     switch (operacao) {
         case PESCOCO:
-            if(key=='+'){
-                pescocoRot = (pescocoRot + 5) % 360;
-            }else{
-                pescocoRot = (pescocoRot - 5) % 360;
-            }
+            if(xRot)
+                pescocoRotX = (pescocoRotX + angulo) % 360;
+            else if(yRot)
+                pescocoRotY = (pescocoRotY + angulo) % 360;
+            else if(zRot)
+                pescocoRotZ = (pescocoRotZ + angulo) % 360;
         break;
 
         case ABDOMEN:
-            if(key=='+'){
-                abdomenRot = (abdomenRot + 5) % 360;
-            }else{
-                abdomenRot = (abdomenRot - 5) % 360;
-            }
+            if(xRot)
+                abdomenRotX = (abdomenRotX + angulo) % 360;
+            else if(yRot)
+                abdomenRotY = (abdomenRotY + angulo) % 360;
+            else if(zRot)
+                abdomenRotZ = (abdomenRotZ + angulo) % 360;
         break;
 
         case BRACO_D:
-            if(key=='+'){
-                bracoDRot = (bracoDRot + 5) % 360;
-            }else{
-                bracoDRot = (bracoDRot - 5) % 360;
-            }
-
+            if(xRot)
+                bracoDRotX = (bracoDRotX + angulo) % 360;
+            else if(yRot)
+                bracoDRotY = (bracoDRotY + angulo) % 360;
+            else if(zRot)
+                bracoDRotZ = (bracoDRotZ + angulo) % 360;
         break;
+
         case ANTEBRACO_D:
-            if(key=='+'){
-                antebracoDRot = (antebracoDRot + 5) % 360;
-            }else{
-                antebracoDRot = (antebracoDRot - 5) % 360;
-            }
-
+            if(xRot)
+                antebracoDRotX = (antebracoDRotX + angulo) % 360;
+            else if(yRot)
+                antebracoDRotY = (antebracoDRotY + angulo) % 360;
+            else if(zRot)
+                antebracoDRotZ = (antebracoDRotZ + angulo) % 360;
         break;
+
         case BRACO_E:
-            if(key=='+'){
-                bracoERot = (bracoERot + 5) % 360;
-            }else{
-                bracoERot = (bracoERot - 5) % 360;
-            }
-
+            if(xRot)
+                bracoERotX = (bracoERotX + angulo) % 360;
+            else if(yRot)
+                bracoERotY = (bracoERotY + angulo) % 360;
+            else if(zRot)
+                bracoERotZ = (bracoERotZ + angulo) % 360;
         break;
+
         case ANTEBRACO_E:
-            if(key=='+'){
-                antebracoERot = (antebracoERot + 5) % 360;
-            }else{
-                antebracoERot = (antebracoERot - 5) % 360;
-            }
-
+            if(xRot)
+                antebracoERotX = (antebracoERotX + angulo) % 360;
+            else if(yRot)
+                antebracoERotY = (antebracoERotY + angulo) % 360;
+            else if(zRot)
+                antebracoERotZ = (antebracoERotZ + angulo) % 360;
         break;
+
         case QUADRIL_D:
-            if(key=='+'){
-                quadrilDRot = (quadrilDRot + 5) % 360;
-            }else{
-                quadrilDRot = (quadrilDRot - 5) % 360;
-            }
-
+            if(xRot)
+                quadrilDRotX = (quadrilDRotX + angulo) % 360;
+            else if(yRot)
+                quadrilDRotY = (quadrilDRotY + angulo) % 360;
+            else if(zRot)
+                quadrilDRotZ = (quadrilDRotZ + angulo) % 360;
         break;
+
         case COXA_D:
-            if(key=='+'){
-                coxaDRot = (coxaDRot + 5) % 360;
-            }else{
-                coxaDRot = (coxaDRot - 5) % 360;
-            }
-
+            if(xRot)
+                coxaDRotX = (coxaDRotX + angulo) % 360;
+            else if(yRot)
+                coxaDRotY = (coxaDRotY + angulo) % 360;
+            else if(zRot)
+                coxaDRotZ = (coxaDRotZ + angulo) % 360;
         break;
+
         case CANELA_D:
-            if(key=='+'){
-                canelaDRot = (canelaDRot + 5) % 360;
-            }else{
-                canelaDRot = (canelaDRot - 5) % 360;
-            }
-
+            if(xRot)
+                canelaDRotX = (canelaDRotX + angulo) % 360;
+            else if(yRot)
+                canelaDRotY = (canelaDRotY + angulo) % 360;
+            else if(zRot)
+                canelaDRotZ = (canelaDRotZ + angulo) % 360;
         break;
+
         case PE_D:
-            if(key=='+'){
-                peDRot = (peDRot + 5) % 360;
-            }else{
-                peDRot = (peDRot - 5) % 360;
-            }
-
+            if(xRot)
+                peDRotX = (peDRotX + angulo) % 360;
+            else if(yRot)
+                peDRotY = (peDRotY + angulo) % 360;
+            else if(zRot)
+                peDRotZ = (peDRotZ + angulo) % 360;
         break;
+
         case QUADRIL_E:
-            if(key=='+'){
-                quadrilERot = (quadrilERot + 5) % 360;
-            }else{
-                quadrilERot = (quadrilERot - 5) % 360;
-            }
-
+            if(xRot)
+                quadrilERotX = (quadrilERotX + angulo) % 360;
+            else if(yRot)
+                quadrilERotY = (quadrilERotY + angulo) % 360;
+            else if(zRot)
+                quadrilERotZ = (quadrilERotZ + angulo) % 360;
         break;
+
         case COXA_E:
-            if(key=='+'){
-                coxaERot = (coxaERot + 5) % 360;
-            }else{
-                coxaERot = (coxaERot - 5) % 360;
-            }
-
+            if(xRot)
+                coxaERotX = (coxaERotX + angulo) % 360;
+            else if(yRot)
+                coxaERotY = (coxaERotY + angulo) % 360;
+            else if(zRot)
+                coxaERotZ = (coxaERotZ + angulo) % 360;
         break;
+
         case CANELA_E:
-            if(key=='+'){
-                canelaERot = (canelaERot + 5) % 360;
-            }else{
-                canelaERot = (canelaERot - 5) % 360;
-            }
-
+            if(xRot)
+                canelaERotX = (canelaERotX + angulo) % 360;
+            else if(yRot)
+                canelaERotY = (canelaERotY + angulo) % 360;
+            else if(zRot)
+                canelaERotZ = (canelaERotZ + angulo) % 360;
         break;
+
         case PE_E:
-            if(key=='+'){
-                peERot = (peERot + 5) % 360;
-            }else{
-                peERot = (peERot - 5) % 360;
-            }
-
-
+            if(xRot)
+                peERotX = (peERotX + angulo) % 360;
+            else if(yRot)
+                peERotY = (peERotY + angulo) % 360;
+            else if(zRot)
+                peERotZ = (peERotZ + angulo) % 360;
         break;
     }
 }
@@ -402,19 +425,19 @@ void key(unsigned char key, int, int) {
             rotaciona('-');
             break;
         case 'x':
-            xRot = 1.0;
-            yRot = 0.0;
-            zRot = 0.0;
+            xRot = 1;
+            yRot = 0;
+            zRot = 0;
             break;
         case 'y':
-            xRot = 0.0;
-            yRot = 1.0;
-            zRot = 0.0;
+            xRot = 0;
+            yRot = 1;
+            zRot = 0;
             break;
         case 'z':
-            xRot = 0.0;
-            yRot = 0.0;
-            zRot = 1.0;
+            xRot = 0;
+            yRot = 0;
+            zRot = 1;
             break;
     }
     glutPostRedisplay();
@@ -429,7 +452,7 @@ void menu(int csgop) {
 void menuCallback(void){
 
     glutCreateMenu(menu);
-    glutAddMenuEntry("Help", HELP);
+    glutAddMenuEntry("MENU", MENU);
     glutAddMenuEntry("Pescoco", PESCOCO);
     glutAddMenuEntry("Abdomen", ABDOMEN);
     glutAddMenuEntry("Braco D", BRACO_D);
@@ -474,7 +497,7 @@ int main(int argc, char **argv) {
 
     glEnable(GL_CULL_FACE);
 
-    glEnable(GL_LIGHTING);
+//    glEnable(GL_LIGHTING);
     glEnable(GL_LIGHT0);
 
     glLightfv(GL_LIGHT0, GL_POSITION, lightpos);
